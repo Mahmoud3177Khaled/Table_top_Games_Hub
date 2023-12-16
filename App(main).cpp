2109e261fd34cd23096b278be19933d36f76a62a
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-12-16 19:38:49 +0200

    Update App(main).cpp
    integrated 2 games out of the 3in one app with selection menu for task 3

--- App(main).cpp
+++ App(main).cpp
@@ -9,6 +9,11 @@
 using namespace std;
 
 int main() {
+
+    int game;
+    cout << "\nWelcome :)\n\nPlease select a game: \n1- Pyramid X O\n2- Connect 4\n3- 5x5 X O\nChoice: ";
+    cin >> game;
+
     int choice;
     Player* players[2];
     players[0] = new Player(1, 'x');
@@ -23,7 +28,26 @@ int main() {
         //Player pointer points to child
         players[1] = new RandomPlayer('o', 5);
 
-    GameManager x_o_game(new Pyramic_X_O_Board(), players);
-    x_o_game.run();
+
+    if(game == 1)
+    {
+        GameManager pyramid_x_o(new Pyramic_X_O_Board(), players);
+        pyramid_x_o.run();
+    }
+    else if(game == 2)
+    {
+        GameManager c4_game(new connect4_board(), players);
+        c4_game.run();
+    }
+    else if(game == 3)
+    {
+
+    }
+    else
+    {
+        cout << "no game selected :(\n";
+    }
+
     system("pause");
+
 }
